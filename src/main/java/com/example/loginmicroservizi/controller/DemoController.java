6a8b47a0f8805d7c6418ffd7c124368d1ad1d9ed
d.bifulco <d.bifulco@dotitsrl.com>
2023-10-11 14:34:55 +0200

    test

---
+++ src/main/java/com/example/loginmicroservizi/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.loginmicroservizi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/prova")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
